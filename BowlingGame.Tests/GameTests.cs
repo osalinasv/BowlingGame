a1a13a0fde4af8c9620b9e21cfc808a530154864
Omar Salinas <osalinasv@outlook.com>
2021-06-16 08:40:45 -0600

    Task #1846643 - Create test cases for acceptance criteria (intentionally left un-refactored for demo purposes)

--- BowlingGame.Tests/GameTests.cs
+++ BowlingGame.Tests/GameTests.cs
@@ -1,9 +1,91 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
 
 namespace BowlingGame.Tests
 {
     [TestClass]
+    [ExcludeFromCodeCoverage]
     public class GameTests
     {
+        private Game game;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            game = new Game();
+        }
+
+        [TestMethod]
+        public void Game_Score_Should_Be_0_When_All_Rolls_Are_0()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(0);
+            }
+
+            // Assert.AreEqual(0, game.Score());
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Game_Score_Should_Be_20_When_All_Rolls_Are_1()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(1);
+            }
+
+            // Assert.AreEqual(20, game.Score());
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Game_Score_Should_Be_13_When_A_Spare_Is_Followed_By_A_3()
+        {
+            game.Roll(6);
+            game.Roll(4);
+
+            game.Roll(3);
+
+            // Assert.AreEqual(13, game.Score());
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Game_Score_Should_Be_16_When_A_Spare_Is_Followed_By_A_3_And_A_0()
+        {
+            game.Roll(6);
+            game.Roll(4);
+
+            game.Roll(3);
+            game.Roll(0);
+
+            // Assert.AreEqual(16, game.Score());
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Game_Score_Should_Be_24_When_A_Strike_Is_Followed_By_A_3_And_A_4()
+        {
+            game.Roll(10);
+
+            game.Roll(3);
+            game.Roll(4);
+
+            // Assert.AreEqual(24, game.Score());
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Game_Score_Should_Be_300_When_All_Rolls_Are_10()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(10);
+            }
+
+            // Assert.AreEqual(30, game.Score());
+            Assert.Inconclusive();
+        }
     }
 }
--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -2,5 +2,13 @@
 {
     public class Game
     {
+        public void Roll(int pins)
+        {
+        }
+
+        public int Score()
+        {
+            return 0;
+        }
     }
 }
